2ba1c9d5597cf85e812628dccebbe79bb2f0a1f4
Maciej Kalinowski <maciej.kalinowski@empik.com>
2018-12-05 13:34:35 +0100

    Clean second solution from day 1 out

--- day1/src/second/Day1Second.kt
+++ day1/src/second/Day1Second.kt
@@ -1,23 +1,23 @@
 package second
 
 import java.io.File
+import java.util.stream.IntStream
+import kotlin.streams.asSequence
 
 fun main() {
   val numbers = getNumbers("./day1/input/input.txt")
-  print(findReapetedFrequency(numbers))
+  print(findRepeatedFrequency(numbers))
 }
 
-fun findReapetedFrequency(numbers: List<Int>): Int {
+fun findRepeatedFrequency(numbers: List<Int>): Int {
   val uniqueFrequencies = HashSet<Int>()
   var sum = 0
-  while (true) {
-    numbers.forEach { number ->
-      sum += number
-      uniqueFrequencies.find { it == sum }?.let {
-        return it
-      } ?: uniqueFrequencies.add(sum)
-    }
-  }
+  return IntStream.iterate(0) { it + 1 }
+    .asSequence()
+    .map { (it % numbers.size) }
+    .map { index -> sum += numbers[index]; sum }
+    .dropWhile { uniqueFrequencies.add(sum) }
+    .first()
 }
 
 fun getNumbers(fileName: String) = File(fileName).readLines().map { it.toInt() }
